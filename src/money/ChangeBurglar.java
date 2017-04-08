f96939b4b2373e87814dffa38fe732d0064e144a
Michael Roschenwimmer <michael.roschenwimmer@gmail.com>
2017-04-08 17:53:58 -0600

    program coming together... not printing results accurately though

--- src/money/ChangeBurglar.java
+++ src/money/ChangeBurglar.java
@@ -5,74 +5,79 @@ import java.util.Scanner;
 public class ChangeBurglar {
 
 	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
 
-		double priceAmount;
-		double tenderedAmount;
-		double tooLittleMoney;
-		double tooMuchMoney;
-
-		int remainingChange;
-		int billsTwenty;
-		int billsTen;
-		int billsFive;
-		int billsOne;
-		int changeQuarter;
-		int changeDime;
-		int changeNickel;
-		int changePenny;
-
-		// moneyIsWrong(); // fix
+		double priceAmount, tenderedAmount, amountReturned;    // declarations
+		int remainingChange;   								   // declarations
 
 		System.out.println("Please enter the price of the merchandise:");
-		Scanner kb = new Scanner(System.in);
-		if (kb != null) {
-			priceAmount = kb.nextDouble();
-			System.out.println("The price of the merchandise is: $" + priceAmount);
-			System.out.println("============================================");
-			System.out.println("Please enter the amount of money tendered by the customer:");
-			tenderedAmount = kb.nextDouble();
-			kb.close();
+		priceAmount = kb.nextDouble();      				   				// cashier enters price of merchandise
+		System.out.println("The price of the merchandise is: $" + priceAmount);
+		System.out.println("============================================");
+		System.out.println("Please enter the amount of money tendered by the customer:");
+		tenderedAmount = kb.nextDouble();                                   // cashier receives X amount from customer
+		kb.close();              									// scanner closed --- no more input
+		amountReturned = tenderedAmount - priceAmount;				// change given back to customer = amount customer gave - price of merchandise
+		remainingChange = (int) amountReturned;		// remaining change for machine to process = amountReturned cased into an (int) in order to multiply by 100 for easier calculations
 
-			double amountReturned = tenderedAmount - priceAmount;
-			remainingChange = (int) (priceAmount * 100);
+		if (tenderedAmount > priceAmount) {          // if amount given by customer to cashier is more than the price of the merchandise then...
+			remainingChange = (int) amountReturned * 100;  
+			int billsTwenty, billsTen, billsFive, billsOne, changeQuarter, changeDime, changeNickel, changePenny;
 
-			billsTwenty = remainingChange / 100;
-			remainingChange = remainingChange / 20;
+			billsTwenty = remainingChange / 2000;
 
-			billsTen = remainingChange / 10;
-			remainingChange = remainingChange / 10;
+			if (billsTwenty > 0) {
+				remainingChange = remainingChange % 2000;
+			}
+			billsTen = remainingChange / 1000;
+			
+			if (billsTen > 0) {
+				remainingChange = remainingChange % 1000;
+			}
+			billsFive = remainingChange / 500;
 
-			billsFive = remainingChange / 5;
-			remainingChange = remainingChange / 5;
-
+			if (billsFive > 0) {
+				remainingChange = remainingChange % 500;
+			}
 			billsOne = remainingChange / 100;
-			remainingChange = remainingChange % 100;
 
+			if (billsOne > 0) {
+				remainingChange = remainingChange % 100;
+			}
 			changeQuarter = remainingChange / 25;
-			remainingChange = remainingChange % 25;
 
+			if (changeQuarter > 0) {
+				remainingChange = remainingChange % 25;
+
+			}
 			changeDime = remainingChange / 10;
-			remainingChange = remainingChange % 10;
 
+			if (changeDime > 0) {
+				remainingChange = remainingChange % 10;
+
+			}
 			changeNickel = remainingChange / 5;
-			remainingChange = remainingChange % 5;
 
-			changePenny = remainingChange / 1;
-			remainingChange = remainingChange % 1;
+			if (changeNickel > 0) {
+				remainingChange = remainingChange % 5;
 
-			System.out.println("Your resulting change to dispense is: $" + amountReturned + " \n" + billsTwenty
+			}
+
+			changePenny = remainingChange;
+
+			System.out.println("Your resulting change to dispense is: $" + (double) amountReturned + " \n" + billsTwenty
 					+ " :Twenty Dollar Bills \n" + billsTen + " :Ten Dollar Bills \n" + billsFive
 					+ " :Five Dollar Bills \n" + billsOne + " :One Dollar Bills \n" + changeQuarter + " :Quarters \n"
 					+ changeDime + " :Dimes \n" + changeNickel + " :Nickels \n" + changePenny + " :Pennies");
 
+			if (tenderedAmount == priceAmount) { // if amount given from customer to cashier is exact then...
+				System.out.print("Exact amount, no change needed.");
+			} else if (tenderedAmount < priceAmount) { // if amount given from customer to cashier is too little then...
+				System.out.print("Please request more money from the customer");
+			}
+
 		}
 	}
-
-	// public static void moneyIsWrong(double tenderedAmount, double
-	// priceAmount) {
-	// while (tenderedAmount != priceAmount) {
-	//
-	// }
 }
 
 // Need a method to define how much change in amount tendered
