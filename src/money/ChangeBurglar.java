b26ce35cf6ea907d957abda72411e367429a4ebe
Michael Roschenwimmer <michael.roschenwimmer@gmail.com>
2017-04-08 11:59:18 -0600

    Making progress, need to add some methods.

--- src/money/ChangeBurglar.java
+++ src/money/ChangeBurglar.java
@@ -5,40 +5,76 @@ import java.util.Scanner;
 public class ChangeBurglar {
 
 	public static void main(String[] args) {
-		
+
 		double priceAmount;
 		double tenderedAmount;
 		double tooLittleMoney;
 		double tooMuchMoney;
-		double remainingChange;
-		
+
+		int remainingChange;
 		int billsTwenty;
+		int billsTen;
+		int billsFive;
 		int billsOne;
 		int changeQuarter;
 		int changeDime;
 		int changeNickel;
 		int changePenny;
-		
-		
-		
-		
-		System.out.println("Please enter the price of the merchandise.");
-		Scanner kb = new Scanner(System.in);
-		if (kb != null)
-		{
-		priceAmount = kb.nextDouble();
-		System.out.println("The price of the merchandise is: $" + priceAmount);
-		System.out.println("============================================");
-		System.out.println("Please enter the amount of money tendered by the customer");
-		tenderedAmount = kb.nextDouble();
-		while (tenderedAmount !=  priceAmount){
-		}
-		kb.close();
-		}
 
+		// moneyIsWrong(); // fix
+
+		System.out.println("Please enter the price of the merchandise:");
+		Scanner kb = new Scanner(System.in);
+		if (kb != null) {
+			priceAmount = kb.nextDouble();
+			System.out.println("The price of the merchandise is: $" + priceAmount);
+			System.out.println("============================================");
+			System.out.println("Please enter the amount of money tendered by the customer:");
+			tenderedAmount = kb.nextDouble();
+			kb.close();
+
+			double amountReturned = tenderedAmount - priceAmount;
+			remainingChange = (int) (priceAmount * 100);
+
+			billsTwenty = remainingChange / 100;
+			remainingChange = remainingChange / 20;
+
+			billsTen = remainingChange / 10;
+			remainingChange = remainingChange / 10;
+
+			billsFive = remainingChange / 5;
+			remainingChange = remainingChange / 5;
+
+			billsOne = remainingChange / 100;
+			remainingChange = remainingChange % 100;
+
+			changeQuarter = remainingChange / 25;
+			remainingChange = remainingChange % 25;
+
+			changeDime = remainingChange / 10;
+			remainingChange = remainingChange % 10;
+
+			changeNickel = remainingChange / 5;
+			remainingChange = remainingChange % 5;
+
+			changePenny = remainingChange / 1;
+			remainingChange = remainingChange % 1;
+
+			System.out.println("Your resulting change to dispense is: $" + amountReturned + " \n" + billsTwenty
+					+ " :Twenty Dollar Bills \n" + billsTen + " :Ten Dollar Bills \n" + billsFive
+					+ " :Five Dollar Bills \n" + billsOne + " :One Dollar Bills \n" + changeQuarter + " :Quarters \n"
+					+ changeDime + " :Dimes \n" + changeNickel + " :Nickels \n" + changePenny + " :Pennies");
+
+		}
 	}
 
+	// public static void moneyIsWrong(double tenderedAmount, double
+	// priceAmount) {
+	// while (tenderedAmount != priceAmount) {
+	//
+	// }
 }
+
 // Need a method to define how much change in amount tendered
 // Need a method to return a correct amount message when the customer provides
 // the exact amount
@@ -50,6 +86,3 @@ public class ChangeBurglar {
 
 // FOR FUN ONLY OPTION Need a method to return a change teller message (pocket
 // the change, i.e. ChangeBurglar) when customer provides too much money
-
-// Notes
-// Maybe use 2d arrays to create the change tables for the amounts of money
