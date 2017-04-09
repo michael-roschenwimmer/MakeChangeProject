4478c4038ec35aff059beaf948ba0efcad9c317b
Michael Roschenwimmer <michael.roschenwimmer@gmail.com>
2017-04-08 18:18:53 -0600

    took out some comments

--- src/money/ChangeBurglar.java
+++ src/money/ChangeBurglar.java
@@ -100,14 +100,6 @@ public class ChangeBurglar {
 	}
 }
 
-// Need a method to define how much change in amount tendered
-// Need a method to return a correct amount message when the customer provides
-// the exact amount
-
-// Need a method to return an error when the customer provides too little money
-
-// Need a method for if amount tendered by customer was too much money to
-// display the amount of dollars and coins that should be given to the customer
 
 // FOR FUN ONLY OPTION Need a method to return a change teller message (pocket
 // the change, i.e. ChangeBurglar) when customer provides too much money
